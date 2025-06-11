17a465297de943ce9e7f719f4aece07a02b09243
Lestare <lestare@bk.ru>
2025-06-11 18:25:00 +0300

    fix: fix Petya's commits

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -1,47 +1,91 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
-
-
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, ExecuteProcess, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
 from launch_ros.actions import Node
 
-
-
 def generate_launch_description():
-
-
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
-    package_name='my_bot' #<--- CHANGE ME
-
+    package_name = 'my_bot'
+    
+    # Используем ваш пользовательский world-файл
+    world_path = os.path.join(
+        get_package_share_directory(package_name), 
+        'worlds',
+        'empty.world'  # Убедитесь, что файл существует в этой папке
+    )
+    
+    # 1. Robot State Publisher
     rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+        PythonLaunchDescriptionSource([
+            os.path.join(
+                get_package_share_directory(package_name),
+                'launch',
+                'rsp.launch.py'
+            )
+        ]),
+        launch_arguments={
+            'use_sim_time': 'true',
+            'use_ros2_control': 'false'
+        }.items()
     )
 
-    # Include the Gazebo launch file, provided by the gazebo_ros package
-    gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-             )
+    # 2. Настраиваем окружение Gazebo с учетом вашей системы
+    gazebo_env = os.environ.copy()
+    
+    # Основные пути Gazebo
+    gazebo_base_path = '/usr/share/gazebo-11'
+    ros_gazebo_path = get_package_share_directory('gazebo_ros')
+    custom_model_path = os.path.join(get_package_share_directory(package_name), 'models')
+    custom_media_path = os.path.join(get_package_share_directory(package_name), 'media')
+    
+    # Формируем пути для Gazebo
+    gazebo_env['GAZEBO_MODEL_PATH'] = ':'.join([
+        gazebo_base_path + '/models',
+        custom_model_path,
+        os.environ.get('GAZEBO_MODEL_PATH', '')
+    ])
+    
+    gazebo_env['GAZEBO_RESOURCE_PATH'] = ':'.join([
+        gazebo_base_path,
+        custom_media_path,
+        ros_gazebo_path,
+        os.environ.get('GAZEBO_RESOURCE_PATH', '')
+    ])
+    
+    gazebo_env['GAZEBO_PLUGIN_PATH'] = ':'.join([
+        '/opt/ros/foxy/lib',
+        os.environ.get('GAZEBO_PLUGIN_PATH', '')
+    ])
 
-    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'my_bot'],
-                        output='screen')
+    # 3. Запуск Gazebo с правильными параметрами
+    gazebo = ExecuteProcess(
+        cmd=[
+            'gazebo',
+            '--verbose',
+            '-s', 'libgazebo_ros_init.so',
+            '-s', 'libgazebo_ros_factory.so',
+            world_path  # Используем ваш пользовательский мир
+        ],
+        output='screen',
+        env=gazebo_env
+    )
 
+    # 4. Спавн робота с задержкой
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-topic', 'robot_description', '-entity', 'my_bot', '-z', '0.1'],
+        output='screen'
+    )
 
+    delayed_spawn = TimerAction(
+        period=5.0,
+        actions=[spawn_entity]
+    )
 
-    # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
-        spawn_entity,
+        delayed_spawn,
     ])
